85fd45a1105bdde964b4beeea3923770fe464116
0364356222hodat <hosydat21052001@gmail.com>
2023-06-15 09:30:54 +0700

    Readfile and sreach country

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,11 +10,42 @@ function writeLog (jsonContent) {
     });
 }
 
-function getAllData (req, res) {
+function createJson (req, res) {
     const listJsonOjb = req.body;
-    // stringify JSON Object
     var jsonContent = JSON.stringify(listJsonOjb);
     writeLog(jsonContent);
     res.send('Successfully!');
-} 
-module.exports = {getAllData}
+}
+
+function getAllData (req, res) {
+    const listJsonOjb = fs.readFileSync("./src/data/output.json");
+    var jsonContent = JSON.parse(listJsonOjb);
+    res.send(jsonContent);
+}
+
+function getDatabyCountry (req, res) {
+    const countryParams = req.params.country;
+    const listJsonOjb = fs.readFileSync("./src/data/output.json");
+    var jsonContent = JSON.parse(listJsonOjb);
+    console.log(jsonContent);
+
+    let newDataImports = jsonContent.children[0].children.filter(data => data.type == 'Imports' );
+    let newDataExports = jsonContent.children[0].children.filter(data => data.type == 'Exports' );
+
+    let dataImportsAfrica =  newDataImports[0].children.filter(data => data.continent == 'Africa');
+    let dataImportsAustralia =  newDataImports[0].children.filter(data => data.continent == 'Australia');
+
+    let DataExportsAfrica = newDataExports[0].children.filter(data => data.continent == 'Africa');
+    let DataExportsAustralia = newDataExports[0].children.filter(data => data.continent == 'Australia');
+
+    let srearchContryImportsAfrica = dataImportsAfrica[0].children.filter(data => data.country == countryParams);
+    let srearchContryImportsAustralia = dataImportsAustralia[0].children.filter(data => data.country == countryParams);
+    let srearchContryExportsAfrica = DataExportsAfrica[0].children.filter(data => data.country == countryParams);
+    let srearchContryExportsAustralia = DataExportsAustralia[0].children.filter(data => data.country == countryParams);
+
+    let newData = [{type : "Imports", children : [...srearchContryImportsAfrica, ...srearchContryImportsAustralia]}
+                    , {type : "Exports", children : [...srearchContryExportsAfrica, ...srearchContryExportsAustralia]}];
+    res.send(newData);
+}
+
+module.exports = {getAllData, createJson, getDatabyCountry}
