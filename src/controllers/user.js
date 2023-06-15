c57103e17226ab8b11b0c1fbd136f3433d2dbf8f
0364356222hodat <hosydat21052001@gmail.com>
2023-06-15 10:47:11 +0700

    fix/bug search data by country

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function writeLog (jsonContent) {
+function writeLog(jsonContent) {
     fs.writeFile("./src/data/output.json", jsonContent, 'utf8', function (err) {
         if (err) {
             console.log("An error occured while writing JSON Object to File.");
@@ -10,42 +10,38 @@ function writeLog (jsonContent) {
     });
 }
 
-function createJson (req, res) {
+function createJson(req, res) {
     const listJsonOjb = req.body;
     var jsonContent = JSON.stringify(listJsonOjb);
     writeLog(jsonContent);
     res.send('Successfully!');
 }
 
-function getAllData (req, res) {
+function getAllData(req, res) {
     const listJsonOjb = fs.readFileSync("./src/data/output.json");
     var jsonContent = JSON.parse(listJsonOjb);
     res.send(jsonContent);
 }
 
-function getDatabyCountry (req, res) {
+function getDatabyCountry(req, res) {
     const countryParams = req.params.country;
     const listJsonOjb = fs.readFileSync("./src/data/output.json");
     var jsonContent = JSON.parse(listJsonOjb);
-    console.log(jsonContent);
+    let newData = [];
 
-    let newDataImports = jsonContent.children[0].children.filter(data => data.type == 'Imports' );
-    let newDataExports = jsonContent.children[0].children.filter(data => data.type == 'Exports' );
+    jsonContent.children[0].children.map(
+        data => data.children.map(
+            data => data.children.map(
+                    data => {
+                        if (data.country == countryParams) {
+                            newData.push(data);
+                        }
+                    }
+                )
+            )
+        );
 
-    let dataImportsAfrica =  newDataImports[0].children.filter(data => data.continent == 'Africa');
-    let dataImportsAustralia =  newDataImports[0].children.filter(data => data.continent == 'Australia');
-
-    let DataExportsAfrica = newDataExports[0].children.filter(data => data.continent == 'Africa');
-    let DataExportsAustralia = newDataExports[0].children.filter(data => data.continent == 'Australia');
-
-    let srearchContryImportsAfrica = dataImportsAfrica[0].children.filter(data => data.country == countryParams);
-    let srearchContryImportsAustralia = dataImportsAustralia[0].children.filter(data => data.country == countryParams);
-    let srearchContryExportsAfrica = DataExportsAfrica[0].children.filter(data => data.country == countryParams);
-    let srearchContryExportsAustralia = DataExportsAustralia[0].children.filter(data => data.country == countryParams);
-
-    let newData = [{type : "Imports", children : [...srearchContryImportsAfrica, ...srearchContryImportsAustralia]}
-                    , {type : "Exports", children : [...srearchContryExportsAfrica, ...srearchContryExportsAustralia]}];
     res.send(newData);
 }
 
-module.exports = {getAllData, createJson, getDatabyCountry}
+module.exports = { getAllData, createJson, getDatabyCountry }
