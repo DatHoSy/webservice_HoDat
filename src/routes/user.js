7e17beaadaafb5def61bb860be23f87210b7eb5a
DatHoSy <73921362+DatHoSy@users.noreply.github.com>
2023-06-19 08:35:13 +0700

    Delete src directory

--- src/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user');
-router.get('/users/create-json', userController.createJson);
-router.get('/users', userController.getAllData);
-router.get('/users/:country', userController.getDatabyCountry);
-
-module.exports = router;
